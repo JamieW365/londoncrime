a0eaf8e63c0b085b36c9c74ca4c6f4c66878f6c3
JamieW365 <jamiewelch1011@gmail.com>
2024-03-28 13:36:27 +0000

    add utils.py

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+
+'''
+Utility functions ot be used throughout project modules
+'''
+
+def get_root_dir() -> str:
+    '''
+    Returns the absolute path of the project root directory.
+    '''
+
+    return str(Path(os.path.abspath(__file__)).parents[5])
